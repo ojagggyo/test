5e3244ef0e504e839405e52c7d95daae21f8f7ab
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2022-10-11 08:41:33 +0900

    Add files via upload

---
+++ bbb.js
@@ -0,0 +1,11 @@
+//関数
+module.exports.funcB = function() {
+  return 'module.exports.funcB !!!';
+};
+
+//オブジェクト
+module.exports.person = {
+  age: 40,
+  name: 'Steemit',
+  title: 'Manager'
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+const a = require('./aaa');
+const b = require('./bbb');
+
+console.log(a.funcA());
+console.log(b.funcB());;
+
+console.log(a.person);
+console.log(b.person);
