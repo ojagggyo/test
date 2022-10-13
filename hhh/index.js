1121e2c89a58df0d7d1fec161e3ecc53dedf2613
yasu hello <helloiamyasu@gmail.com>
2022-10-13 14:06:39 +0900

    m

--- hhh/index.js
+++ hhh/index.js
@@ -6,35 +6,36 @@ const client = new dsteem.Client('https://api.steemit.com');
 //filter change selection function
 getPosts = async () => {
 
-  //filter change selection function
-  const filter = 'created';
-  const query = {
-          tag: 'japanese',
-          limit: 1,
-  };
-  
-  //console.log('Post assembled.\nFilter:', filter, '\nQuery:', query);
-  
-  client.database
-      .getDiscussions(filter, query)
-      .then(result => {
-          //console.log('Response received:', result);
+    //filter change selection function
+    const filter = 'created';
+    const query = {
+            tag: 'japanese',
+            limit: 1,
+    };
 
-          var authorList = [];
-          result.forEach(post => {
-              const json = JSON.parse(post.json_metadata);
-              //const image = json.image ? json.image[0] : '';
-              //const title = post.title;
-              const author = post.author;
-              authorList.push(author);
-          return authorList.join(",");
-      })
-      .catch(err => {
-          //console.log(err);
-          return err;
-      });
-};
+    //console.log('Post assembled.\nFilter:', filter, '\nQuery:', query);
 
+    client.database
+        .getDiscussions(filter, query)
+        .then(result => {
+            //console.log('Response received:', result);
+
+            var authorList = [];
+            result.forEach(post => {
+                const json = JSON.parse(post.json_metadata);
+                //const image = json.image ? json.image[0] : '';
+                //const title = post.title;
+                const author = post.author;
+                authorList.push(author);
+                return authorList.join(",");
+            })
+
+        .catch(err => {
+            //console.log(err);
+            return err;
+        });
+    }
+}
 
 
 function log(msg) { 
