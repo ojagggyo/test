18daafacf18522a0c37d729e57e950c03aea73ba
yasu hello <helloiamyasu@gmail.com>
2022-10-14 10:46:51 +0900

    m

--- 221015_1/src/index.js
+++ 221015_1/src/index.js
@@ -2,10 +2,10 @@ function log(msg) {
     console.log(new Date().toString() + ' - ' + msg); 
 }
 
-// window.getPosts = async () => {
-//     document.getElementById('aaa').innerHTML = log('Hello!');  
-// };
-
-window.hello = () => {
+window.getPosts = async () => {
     document.getElementById('aaa').innerHTML = log('Hello!');  
 };
+
+// window.hello = () => {
+//     document.getElementById('aaa').innerHTML = log('Hello!');  
+// };
