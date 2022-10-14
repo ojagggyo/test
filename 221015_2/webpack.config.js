0c87bbeb5fbab2ecaa70a65353ef28e4cdeba04c
ojagggyo <ojagggyo@gmail.com>
2022-10-14 19:12:35 +0900

    m

--- 221015_2/webpack.config.js
+++ 221015_2/webpack.config.js
@@ -9,8 +9,7 @@ module.exports = {
     // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
     mode: 'development',
     // エントリーポイントの設定
-    //entry: './src/app.js',
-    entry: './src/index.js',
+    entry: './src/app.js',
     // 出力の設定
     output: {
     // 出力するファイル名
