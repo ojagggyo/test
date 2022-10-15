28c9ac67980a9644061e20765fa6f2a69a87d2ca
ojagggyo <ojagggyo@gmail.com>
2022-10-15 19:35:56 +0900

    m

--- list/webpack.config.js
+++ list/webpack.config.js
@@ -5,7 +5,9 @@ module.exports = {
     // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
     mode: 'development',
     // エントリーポイントの設定
-    entry: './src/app.js',
+    //entry: './src/app.js',
+    // Uncaught (in promise) ReferenceError: regeneratorRuntime is not defined
+    entry: ['regenerator-runtime/runtime.js', './src/app.js'],
     // 出力の設定
     output: {
         // 出力するファイル名
@@ -13,6 +15,4 @@ module.exports = {
         // 出力先のパス（絶対パスを指定する必要がある）
         path: path.join(__dirname, 'public')
     },
-    // Uncaught (in promise) ReferenceError: regeneratorRuntime is not defined
-    //entry: ['regenerator-runtime/runtime.js', './public/bundle.js']
 };
