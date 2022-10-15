9a5cde20cf1e5be20aca591f59b61a22e572c4c2
ojagggyo <ojagggyo@gmail.com>
2022-10-16 08:07:58 +0900

    m

--- 221016/app.js
+++ 221016/app.js
@@ -12,51 +12,47 @@ const authorAcount = {
 };
 
 createPost = async () => {
-        //get private key
-        const privateKey = authorAcount.privateKey;
-        //get account name
-        const account = authorAcount.acountName;
-        //for content
-        const time = new Date().getTime();
-        //get title
-        const title = `テスト ${time}`;
-        //get body
-        const body = `本文 ${time}`;
-        //get tags and convert to array list
-        const tags = 'yasupal';
-        const taglist = tags.split(' ');
-        //make simple json metadata including only tags
-        const json_metadata = JSON.stringify({ tags: taglist });
-        //generate random permanent link for post
-        const permlink = Math.random()
-            .toString(36)
-            .substring(2);
-    
-        client.broadcast
-            .comment(
-                {
-                    author: account,
-                    body: body,
-                    json_metadata: json_metadata,
-                    parent_author: '',
-                    parent_permlink: tags,
-                    permlink: permlink,
-                    title: title,
-                },
-                privateKey
-            )
-            .then(
-                function(result) {
-                    console.log(result);
-                    
-                    console.log(result.block_num);                  
-               },
-                function(error) {
-                    console.error(error);
-                }
-            );
+    //get private key
+    const privateKey = authorAcount.privateKey;
+    //get account name
+    const account = authorAcount.acountName;
+    //for content
+    const time = new Date().getTime();
+    //get title
+    const title = `テスト ${time}`;
+    //get body
+    const body = `本文 ${time}`;
+    //get tags and convert to array list
+    const tags = 'yasupal';
+    const taglist = tags.split(' ');
+    //make simple json metadata including only tags
+    const json_metadata = JSON.stringify({ tags: taglist });
+    //generate random permanent link for post
+    const permlink = Math.random()
+        .toString(36)
+        .substring(2);
+
+    client.broadcast
+        .comment(
+            {
+                author: account,
+                body: body,
+                json_metadata: json_metadata,
+                parent_author: '',
+                parent_permlink: tags,
+                permlink: permlink,
+                title: title,
+            },
+            privateKey
+        )
+        .then(
+            function(result) {
+                console.log(result);
+            },
+            function(error) {
+                console.error(error);
+            }
+        );
 };
-
-
 
 createPost();
