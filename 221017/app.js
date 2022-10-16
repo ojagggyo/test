f2ee2c7111e5f68f0265dee0e6ead0da37181cb1
ojagggyo <ojagggyo@gmail.com>
2022-10-16 18:42:19 +0900

    m

--- 221017/app.js
+++ 221017/app.js
@@ -5,7 +5,7 @@ const client = new dsteem.Client('https://api.steemit.com');
 
 //submitTab function from html input
 const max = 100;
-const tagSearch = "jp";
+const tagSearch = "";
 
 submitTag = async () => {
 
