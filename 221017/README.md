5f83fdb7a8928eae9499ff27c972d961c2776d69
ojagggyo <ojagggyo@gmail.com>
2022-10-16 15:39:16 +0900

    m

---
+++ 221017/README.md
@@ -0,0 +1,7 @@
+#[JS: Get Post Details](https://developers.steem.io/tutorials-javascript/get_post_details)
+
+初期化
+npm init
+
+実行
+node app.js
---
+++ 221017/app.js
@@ -0,0 +1,43 @@
+const dsteem = require('dsteem');
+// const fs = require("fs");
+
+// var config = JSON.parse(fs.readFileSync("config.json"));
+
+//connect to server which is connected to the network/production
+const client = new dsteem.Client('https://api.steemit.com');
+
+// const authorAcount = {
+//     acountName: config.acount_name,
+//     privateKey: dsteem.PrivateKey.fromString(config.posting_key)
+// };
+
+//fetch list of trending posts
+async function main() {
+    const query = {
+        tag: 'japanese',
+        limit: 5,
+        truncate_body: 1,
+    };
+    client.database
+        .getDiscussions('created', query)
+        .then(result => {
+            var posts = [];
+            result.forEach(post => {
+                console.log(post);
+                const json = JSON.parse(post.json_metadata);
+                const image = json.image ? json.image[0] : '';
+                const title = post.title;
+                const author = post.author;
+                const permlink = post.permlink;
+                const created = new Date(post.created).toDateString();
+
+                console.log(`${author},${permlink}`);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            // alert('Error occured, please reload the page');
+        });
+}
+//catch error messages
+main().catch(console.error);
