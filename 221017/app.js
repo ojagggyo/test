12063409038ceb26561cfb00a1e61464e2919390
ojagggyo <ojagggyo@gmail.com>
2022-10-17 09:40:27 +0900

    m

--- 221017/app.js
+++ 221017/app.js
@@ -3,7 +3,6 @@ const dsteem = require('dsteem');
 //connect to server which is connected to the network/production
 const client = new dsteem.Client('https://api.steemit.com');
 
-
 const fs = require("fs");
 var config = JSON.parse(fs.readFileSync("config.json"));
 const authorAcount = {
@@ -13,14 +12,27 @@ const authorAcount = {
 };
 
 
-edit_content = async (author, permlink, parent_permlink, tags, body) => {
+// 差分を作成する。
+const diff_match_patch = require('diff-match-patch');
+const dmp = new diff_match_patch();
 
-    //console.log(" *** edit_content ***");
-    //console.log(author);
-    //console.log(permlink);
-    //console.log(parent_permlink);
-    //console.log(tags);
-    //console.log(body);
+function createPatch(oldText, newText) {
+    if (!oldText && oldText === '') return undefined;
+    const patch_make = dmp.patch_make(oldText, newText);
+    const patch = dmp.patch_toText(patch_make);
+    return patch;
+}
+
+
+edit_content = async (author, permlink, parent_permlink, tags, old_body, new_body) => {
+
+    console.log(" *** edit_content ***");
+    console.log(author);
+    console.log(permlink);
+    console.log(parent_permlink);
+    console.log(tags);
+    console.log(old_body);
+    console.log(new_body);
 
     //get private key
     const privateKey = authorAcount.privateKey;
@@ -33,6 +45,18 @@ edit_content = async (author, permlink, parent_permlink, tags, body) => {
     const taglist = tags;
     //make simple json metadata including only tags
     const json_metadata = JSON.stringify({ tags: taglist });
+
+
+    //computes a list of patches to turn o_body to edited_body
+    const patch = createPatch(old_body, new_body);
+
+    //check if patch size is smaller than edited content itself
+    if (patch && patch.length < new Buffer(new_body, 'utf-8').length) {
+        body = patch;//差分
+    } else {
+        body = new_body;
+    }
+
 
     client.broadcast
         .comment(
@@ -72,13 +96,13 @@ async function getLatestPost() {
         .then(result => {
             result.forEach(post => {
                 //console.log(post);
-                const body = post.body + " " + new Date().getTime();
+                const new_body = post.body + " " + new Date().getTime();//本文を編集する。
                 const json = JSON.parse(post.json_metadata);
                 //console.log(json);
                 tags = ('tags' in json) ? json.tags : [];
                 //console.log(tags);
                 //console.log(`author=${post.author},permlink=${post.permlink},parent_permlink=${post.parent_permlink},tags=${tags.join(".")}`);
-                edit_content(post.author, post.permlink, post.parent_permlink, tags, body);
+                edit_content(post.author, post.permlink, post.parent_permlink, tags, body, new_body);
             });
         })
         .catch(err => {
