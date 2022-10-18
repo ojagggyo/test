f595a018e97db12b17a564c9b0738682c65c0269
ojagggyo <ojagggyo@gmail.com>
2022-10-19 00:14:55 +0900

    m

---
+++ sharp/app.js
@@ -0,0 +1,4 @@
+const sharp = require("sharp");
+
+
+const promise = sharp("https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png");
