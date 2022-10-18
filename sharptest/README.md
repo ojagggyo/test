8478b865a4d322137e7d921ba62f89d75eef7653
ojagggyo <ojagggyo@gmail.com>
2022-10-19 00:53:43 +0900

    m

--- sharptest/README.md
+++ sharptest/README.md
@@ -1,4 +1,8 @@
 # 画像を処理する
 
-
+NG
 npm install sharp
+npm install sharp@0.31.1
+
+OK?
+npm install sharp@0.23.2 
--- sharptest/app.js
+++ sharptest/app.js
@@ -1,9 +1,28 @@
-const sharp = require("sharp");
+// const sharp = require("sharp");
 
 
-const promise = sharp("https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png");
+// const promise = sharp("https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png");
 
 
-promise.toFile( "./aaa.png" , ( err , info ) =>{
-    if( err ) { console.error(err) }
-});
+// promise.toFile( "./aaa.png" , ( err , info ) =>{
+//     if( err ) { console.error(err) }
+// });
+
+
+const download = (uri , filename ) => {
+    return new Promise((resolve, reject) =>
+      https
+        .request(uri, (res) => {
+          res
+            .pipe(createWriteStream(filename))
+            .on("close", resolve)
+            .on("error", reject);
+        })
+        .end()
+    );
+  };
+  
+  download(
+    "https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png",
+    "Charizard-gmax.png"
+  ).then(() => console.log("done"));
