b18923fa393aeb80d4990bef9bc528853fa4c88a
ojagggyo <ojagggyo@gmail.com>
2022-10-19 13:54:43 +0900

    m

--- sharptest/README.md
+++ sharptest/README.md
@@ -13,3 +13,6 @@ https://qiita.com/eyuta/items/ae5115091d3865e0c530
 
 ??
 https://qiita.com/watatakahashi/items/4b456971ae6dc3038569
+
+??
+https://www.web-dev-qa-db-ja.com/ja/javascript/nodejs%E3%81%A7%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%82%92%E3%83%80%E3%82%A6%E3%83%B3%E3%83%AD%E3%83%BC%E3%83%89%E3%81%99%E3%82%8B%E6%96%B9%E6%B3%95%EF%BC%88%E4%BB%96%E7%A4%BE%E3%81%AE%E3%83%A9%E3%82%A4%E3%83%96%E3%83%A9%E3%83%AA%E3%82%92%E4%BD%BF%E7%94%A8%E3%81%9B%E3%81%9A%E3%81%AB%EF%BC%89%EF%BC%9F/1067648776/
--- sharptest/app.js
+++ sharptest/app.js
@@ -10,11 +10,50 @@
 
 
 
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
+// const http = require('http');
+// const https = require('https');
+// const fs = require('fs');
 
-const file = fs.createWriteStream("file.jpg");
-const request = https.get("https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png", function(response) {
-  response.pipe(file);
-});
+// const file = fs.createWriteStream("file.jpg");
+// const request = https.get("https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png", function(response) {
+//   response.pipe(file);
+// });
+
+
+
+
+// const https = require('https');
+// var fs = require('fs');
+// url = "https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png";
+// dest = "file.jpg";
+
+// var download = function(url, dest, cb) {
+//   var file = fs.createWriteStream(dest);
+//   var request = https.get(url, function(response) {
+//     response.pipe(file);
+//     file.on('finish', function() {
+//       file.close(cb);  // close() is async, call cb after close completes.
+//     });
+//   }).on('error', function(err) { // Handle errors
+//     fs.unlink(dest); // Delete the file async. (But we don't check the result)
+//     if (cb) cb(err.message);
+//   });
+// };
+
+// download();
+
+
+
+var request = require('request');
+var fs = require('fs');
+
+var url = 'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png';
+
+request(
+    {method: 'GET', url: url, encoding: null},
+    function (error, response, body){
+        if(!error && response.statusCode === 200){
+            fs.writeFileSync('a.png', body, 'binary');
+        }
+    }
+);
