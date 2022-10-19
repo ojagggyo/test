0b5ef573d3e7588d193046eaa9ae31d07ee3dcd7
ojagggyo <ojagggyo@gmail.com>
2022-10-19 20:05:23 +0900

    m

--- sharp3/app.js
+++ sharp3/app.js
@@ -43,9 +43,9 @@ for (let index = 0; index < urls.length; index++) {
 
 let payload = []
 for (let index = 0; index < urls.length; index++) {
-    let x = index % 2;
-    let y = index / 2;
-    a = {input: `./${index + 1}.png`, top: y * 1, left: x * 200};
+    //let x = index % 2;
+    //let y = 
+    a = {input: `./${index + 1}.png`, top: `${0}`, left: `${index % 2}`};
     payload.push(a);
 }
 
