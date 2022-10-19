0e4484fb7bdab45822f2bb97f676b218ba7cdac2
ojagggyo <ojagggyo@gmail.com>
2022-10-19 14:04:28 +0900

    m

--- sharptest/app.js
+++ sharptest/app.js
@@ -1,47 +1,4 @@
-// const sharp = require("sharp");
-
-
-// const promise = sharp("https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png");
-
-
-// promise.toFile( "./aaa.png" , ( err , info ) =>{
-//     if( err ) { console.error(err) }
-// });
-
-
-
-// const http = require('http');
-// const https = require('https');
-// const fs = require('fs');
-
-// const file = fs.createWriteStream("file.jpg");
-// const request = https.get("https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png", function(response) {
-//   response.pipe(file);
-// });
-
-
-
-
-// const https = require('https');
-// var fs = require('fs');
-// url = "https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png";
-// dest = "file.jpg";
-
-// var download = function(url, dest, cb) {
-//   var file = fs.createWriteStream(dest);
-//   var request = https.get(url, function(response) {
-//     response.pipe(file);
-//     file.on('finish', function() {
-//       file.close(cb);  // close() is async, call cb after close completes.
-//     });
-//   }).on('error', function(err) { // Handle errors
-//     fs.unlink(dest); // Delete the file async. (But we don't check the result)
-//     if (cb) cb(err.message);
-//   });
-// };
-
-// download();
-
+const sharp = require("sharp");
 
 
 var request = require('request');
@@ -57,3 +14,10 @@ request(
         }
     }
 );
+
+const promise = sharp("./images/a.png");
+
+promise.toFile( "./aaa.png" , ( err , info ) =>{
+    if( err ) { console.error(err) }
+});
+
