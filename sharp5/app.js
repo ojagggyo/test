0d97078319d306cf0c78e19d032b8a4a352668f3
ojagggyo <ojagggyo@gmail.com>
2022-10-20 09:45:41 +0900

    m

--- sharp5/app.js
+++ sharp5/app.js
@@ -10,14 +10,14 @@ var fs = require('fs');
 var urls = [
     'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png',
     'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmW75RTjrArTdMMXBdr5eyrBhp7ZeFHgDYXpVKWb7NDojC/image.png',
-    //'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmczvYkSKu5U1L1go8UEFy5Y2H3CNADpqpVyKnStrgBUey/image.png',
-    //'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png',
-    //'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmW75RTjrArTdMMXBdr5eyrBhp7ZeFHgDYXpVKWb7NDojC/image.png',
-    //'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmczvYkSKu5U1L1go8UEFy5Y2H3CNADpqpVyKnStrgBUey/image.png',
-    ///'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png',
-    //'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png',
-    //'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmW75RTjrArTdMMXBdr5eyrBhp7ZeFHgDYXpVKWb7NDojC/image.png',
-    //'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmczvYkSKu5U1L1go8UEFy5Y2H3CNADpqpVyKnStrgBUey/image.png',
+    'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmczvYkSKu5U1L1go8UEFy5Y2H3CNADpqpVyKnStrgBUey/image.png',
+    'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png',
+    'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmW75RTjrArTdMMXBdr5eyrBhp7ZeFHgDYXpVKWb7NDojC/image.png',
+    'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmczvYkSKu5U1L1go8UEFy5Y2H3CNADpqpVyKnStrgBUey/image.png',
+    'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png',
+    'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmdA56beW1LVbnL28qphDBLnSYTZmmcbt1wdmhX5RqQSnf/image.png',
+    'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmW75RTjrArTdMMXBdr5eyrBhp7ZeFHgDYXpVKWb7NDojC/image.png',
+    'https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmczvYkSKu5U1L1go8UEFy5Y2H3CNADpqpVyKnStrgBUey/image.png',
   
 ];
 
@@ -41,65 +41,62 @@ for (let index = 0; index < urls.length; index++) {
 (async ()=>{//非同期開始
 //--------------------
 
-const sharp = require('sharp')
+    const sharp = require('sharp')
 
-const n = urls.length;
-const image_width = 200;
-const image_height = 150;
-const x = parseInt(Math.sqrt(n - 1)) + 1;
-const y = parseInt((n - 1) / x) + 1;
-console.log(`n=${n}`);
-console.log(`x=${x},y=${y}`);
+    const n = urls.length;
+    const image_width = 200;
+    const image_height = 150;
+    const x = parseInt(Math.sqrt(n - 1)) + 1;
+    const y = parseInt((n - 1) / x) + 1;
+    console.log(`n=${n}`);
+    console.log(`x=${x},y=${y}`);
 
-//
-console.log("ダウンロードした画像をリサイズする。");
-for (let index = 0; index < urls.length; index++) {
-    await sharp(`./images/${index + 1}.png`)
-        .resize(
-            {
-                width: image_width, 
-                height: image_height, 
-                //fit: 'contain'
-            })
-        .toFile(`./resize/${index + 1}.png`);
+    //
+    console.log("ダウンロードした画像をリサイズする。");
+    for (let index = 0; index < urls.length; index++) {
+        await sharp(`./images/${index + 1}.png`)
+            .resize(
+                {
+                    width: image_width, 
+                    height: image_height, 
+                    //fit: 'contain'
+                })
+            .toFile(`./resize/${index + 1}.png`);
 
-    console.log("1秒スリープ");
-    await sleep(1000);
-}
+        console.log("1秒スリープ");
+        await sleep(1000);
+    }
 
+    //
+    console.log("composite用のデータを作成する。");
+    let payload = []
+    for (let index = 0; index < urls.length; index++) {
+        const dx = index % x;
+        const dy = parseInt(index / x);
+        console.log(`dx=${dx},dy=${dy}`);
+        a = {
+            input: `./resize/${index + 1}.png`, 
+            top: dy * image_height, 
+            left: dx * image_width,
+        };
+        payload.push(a);
+    }
 
-//
-console.log("composite用のデータを作成する。");
-let payload = []
-for (let index = 0; index < urls.length; index++) {
-    const dx = index % x;
-    const dy = parseInt(index / x);
-    console.log(`dx=${dx},dy=${dy}`);
-    a = {
-        input: `./resize/${index + 1}.png`, 
-        top: dy * image_height, 
-        left: dx * image_width,
-    };
-    payload.push(a);
-}
-
-
-//
-console.log("合成する。");
-const s = await sharp(
-    {//背景
-        create: {
-            width: x * image_width,
-            height: y * image_height,
-            channels: 4,
-            background: { r: 255, g: 100, b: 100, alpha: 0.1 }//色を指定する。
-        }
-    });
-for (let index = 0; index < urls.length; index++) {
-    await s.composite(payload)
-}
-await s.toFile(`./outout.png`);
-
+    //
+    console.log("合成する。");
+    const s = await sharp(
+        {//背景
+            create: {
+                width: x * image_width,
+                height: y * image_height,
+                channels: 4,
+                background: { r: 255, g: 100, b: 100, alpha: 0.1 }//色を指定する。
+            }
+        });
+    for (let index = 0; index < urls.length; index++) {
+        await s.composite(payload)
+    }
+    await s.toFile(`./outout.png`);
 
 //--------------------
 })();//非同期終了
