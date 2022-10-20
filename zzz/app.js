4c16700eca1a9d1d928d022082945b09fe4b8821
ojagggyo <ojagggyo@gmail.com>
2022-10-20 12:40:44 +0900

    m

--- zzz/app.js
+++ zzz/app.js
@@ -6,11 +6,11 @@
 }
 
 async function main() {
-    console.log("aaa") /
+    console.log("aaa") 
     console.log(1 + 非同期処理()) // 1[object Promise]
-    console.log("bbb") /
+    console.log("bbb") 
     console.log(1 + await 非同期処理()) // 2
-    console.log("ccc") /
+    console.log("ccc") 
 }
 
 main();
