68c280e6f0018d32575447b9e6c49e09ce327c4d
ojagggyo <ojagggyo@gmail.com>
2022-10-20 14:55:14 +0900

    m

--- sharp6/app1.js
+++ sharp6/app1.js
@@ -1,105 +1,73 @@
-const app = require('./app.js');
+const dsteem = require('dsteem');
+//const fs = require('fs');
+//const request = require('request');
 
-// いい感じにPromiseでラップする
-const sleep = (m) => {
-    return new Promise((resolve) => setTimeout(resolve, m));
-  };
-  
+//connect to server which is connected to the network/production
+const client = new dsteem.Client('https://api.steemit.com');
 
-// ダウンロード
-var request = require('request');
-var fs = require('fs');
+function getDateString(date){
+    return `${date.getMonth()+1}/${date.getDay()} ${date.getHours()}:${date.getMinutes()}`;
+}
+function log(msg) { 
+    console.log(new Date().toString() + ' - ' + msg); 
+}
 
 
-//
-async function main(){
-    await app.getPosts()
+//ミュートしたいアカウント
+const muteList = ["bukitcantik"];
+
+
+//filter change selection function
+module.exports.getPosts = async () => {
+return new Promise((resolve, reject) => {
+        
+    console.log('*** getPosts開始 ***');
+
+    let urls = [];
+    const filter = "created";
+    const query = {
+        //tag: 'japanese',
+        tag: 'hive-161179',
+        limit: 40,
+        truncate_body: 1//本文を1文字だけ取得
+    };
+
+    client.database
+        .getDiscussions(filter, query)
         .then(result => {
-            console.log(result);
-            sub(result);
-        });   
-}
+            console.log('Response received:', result);
 
+            if (result) {
+                
+                let index = 0;
+                result.forEach(post => {
+                    const json = JSON.parse(post.json_metadata);
+                    //const image = json.image ? json.image[0] : '';
 
-async function sub(urls){
-//--------------------
-//(async ()=>{//非同期開始
-//--------------------
-   
-    console.log("画像をダウンロードする。");
-    for (let index = 0; index < urls.length; index++) {
-        const url = urls[index];
-        request(
-            {method: 'GET', url: url, encoding: null},
-            function (error, response, body){
-                if(!error && response.statusCode === 200){
-                    fs.writeFileSync(`./images/${index + 1}.png`, body, 'binary');
-                }
+                    if(json.image){
+                        
+                        console.log("urls.push(json.image[0]);");
+
+                        urls.push(json.image[0]);
+                    }
+
+                });
+
+                return resolve(urls);
+            } else {
+                return resolve(urls);
             }
-        );
-    }
+      
 
-    const sharp = require('sharp')
-
-    const n = urls.length;
-    const image_width = 200;
-    const image_height = 150;
-    const x = parseInt(Math.sqrt(n - 1)) + 1;
-    const y = parseInt((n - 1) / x) + 1;
-    console.log(`n=${n}`);
-    console.log(`x=${x},y=${y}`);
-
-    //
-    console.log("ダウンロードした画像をリサイズする。");
-    for (let index = 0; index < urls.length; index++) {
-        await sharp(`./images/${index + 1}.png`)
-            .resize(
-                {
-                    width: image_width, 
-                    height: image_height, 
-                    //fit: 'contain'
-                })
-            .toFile(`./resize/${index + 1}.png`);
-
-        console.log("0.1秒スリープ");
-        await sleep(100);
-    }
-
-    //
-    console.log("composite用のデータを作成する。");
-    let payload = []
-    for (let index = 0; index < urls.length; index++) {
-        const dx = index % x;
-        const dy = parseInt(index / x);
-        console.log(`dx=${dx},dy=${dy}`);
-        a = {
-            input: `./resize/${index + 1}.png`, 
-            top: dy * image_height, 
-            left: dx * image_width,
-        };
-        payload.push(a);
-    }
-
-    //
-    console.log("合成する。");
-    const s = await sharp(
-        {//背景
-            create: {
-                width: x * image_width,
-                height: y * image_height,
-                channels: 4,
-                background: { r: 255, g: 100, b: 100, alpha: 0.1 }//色を指定する。
-            }
+        })
+        .catch(err => {
+            console.log(err);
+            //alert(`Error:${err}, try again`);
+            return reject(urls);
         });
-    for (let index = 0; index < urls.length; index++) {
-        await s.composite(payload)
-    }
-    await s.toFile(`./out.png`);
-
-}
-//--------------------
-//})();//非同期終了
-//--------------------
 
 
-main();
+    console.log('*** getPosts終了 ***');
+
+});
+};
