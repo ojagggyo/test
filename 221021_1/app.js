8e41b8eff8c796016045f0a97a72a79286aa08bd
ojagggyo <ojagggyo@gmail.com>
2022-10-21 19:00:27 +0900

    m

--- 221021_1/app.js
+++ 221021_1/app.js
@@ -3,30 +3,7 @@ const dsteem = require('dsteem');
 //connect to server which is connected to the network/production
 const client = new dsteem.Client('https://api.steemit.com');
 
-
-
-// get_accounts = (accountName) => Promise.all(
-//     client.database
-//         .call('get_accounts', [[accountName]])
-//             .then(result => {
-//                 console.log('get_accounts then');
-//                 if(result.length > 0){
-//                     console.log(result[0]);
-//                     console.log("");
-//                 }
-//                 return 123;
-//             })
-//             .then(result => {
-//                 console.log('get_accounts then 2');
-//                 console.log(result);
-//                 console.log("");
-//             }
-//             )
-// )
-
-get_accounts = async (accountName) => {
-    console.log('get_accounts start');
-    
+get_accounts = async (accountName) => { 
     client.database
     .call('get_accounts', [[accountName]])
         .then(result => {
@@ -35,22 +12,16 @@ get_accounts = async (accountName) => {
                 console.log(result[0]);
                 console.log("");
             }
-            return 123;
         })
-        .all(   console.log("all"));
-
-    console.log('get_accounts end');
 };
 
-
 //コマンドパラメータ取得
-let accountName = "yasu";//デフォルト
+let accountName = "";//デフォルト
 if(process.argv.length > 2){
     accountName = process.argv[2];
 }else{
    console.log('node app.js accountName'); 
+   return;
 }
 
-console.log('main start');
 get_accounts(accountName);
-console.log('main end');
---
+++ 221022/app.js
@@ -0,0 +1,55 @@
+const dsteem = require('dsteem');
+
+//connect to server which is connected to the network/production
+const client = new dsteem.Client('https://api.steemit.com');
+
+
+
+// get_accounts = (accountName) => Promise.all(
+//     client.database
+//         .call('get_accounts', [[accountName]])
+//             .then(result => {
+//                 console.log('get_accounts then');
+//                 if(result.length > 0){
+//                     console.log(result[0]);
+//                     console.log("");
+//                 }
+//                 return 123;
+//             })
+//             .then(result => {
+//                 console.log('get_accounts then 2');
+//                 console.log(result);
+//                 console.log("");
+//             }
+//             )
+// )
+
+get_accounts = async (accountName) => {
+    console.log('get_accounts start');
+    
+    client.database
+    .call('get_accounts', [[accountName]])
+        .then(result => {
+            console.log('get_accounts then ');
+            if(result.length > 0){
+                console.log(result[0]);
+                console.log("");
+            }
+            return 123;
+        })
+
+    console.log('get_accounts end');
+};
+
+
+//コマンドパラメータ取得
+let accountName = "yasu";//デフォルト
+if(process.argv.length > 2){
+    accountName = process.argv[2];
+}else{
+   console.log('node app.js accountName'); 
+}
+
+console.log('main start');
+get_accounts(accountName);
+console.log('main end');
