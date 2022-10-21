3447621ac8193322413d1151a26b4000f349d022
ojagggyo <ojagggyo@gmail.com>
2022-10-21 19:48:36 +0900

    m

--- 221022/app.js
+++ 221022/app.js
@@ -5,60 +5,54 @@ const client = new dsteem.Client('https://api.steemit.com');
 
 
 
-// get_accounts = (accountName) => Promise.all(
+
+
+
+// 
+// get_accounts = async (accountName) => {
+//     console.log('get_accounts start');
+    
 //     client.database
-//         .call('get_accounts', [[accountName]])
-//             .then(result => {
-//                 console.log('get_accounts then');
-//                 if(result.length > 0){
-//                     console.log(result[0]);
-//                     console.log("");
-//                 }
-//                 return 123;
-//             })
-//             .then(result => {
-//                 console.log('get_accounts then 2');
-//                 console.log(result);
+//     .call('get_accounts', [[accountName]])
+//         .then(result => {
+//             console.log('get_accounts then ');
+//             if(result.length > 0){
+//                 console.log(result[0]);
 //                 console.log("");
 //             }
-//             )
-// )
+//             return 123;
+//         }).then(result => {
+//             console.log('get_accounts then 2');
+//         });
 
-get_accounts = async (accountName) => {
+//     console.log('get_accounts end');
+// };
+
+get_accounts = (accountName) => {
     console.log('get_accounts start');
     
     client.database
-    .call('get_accounts', [[accountName]])
-        .then(result => {
-            console.log('get_accounts then ');
-            if(result.length > 0){
-                console.log(result[0]);
-                console.log("");
-            }
-            return 123;
-        }).then(result => {
-            console.log('get_accounts then 2');
-        });
-
+    .call('get_accounts', [[accountName]]);
+ 
     console.log('get_accounts end');
 };
 
 
-//コマンドパラメータ取得
-let accountName = "yasu";//デフォルト
-if(process.argv.length > 2){
-    accountName = process.argv[2];
-}else{
-   console.log('node app.js accountName'); 
-}
-
 
 async function main()
 {
+    //コマンドパラメータ取得
+    let accountName = "yasu";//デフォルト
+    if(process.argv.length > 2){
+        accountName = process.argv[2];
+    }else{
+    console.log('node app.js accountName'); 
+    }
+
     console.log('main start');
-    await get_accounts(accountName);
+    const ret = get_accounts(accountName);
+    console.log(ret);
     console.log('main end');
-    
 }
 
 main();
