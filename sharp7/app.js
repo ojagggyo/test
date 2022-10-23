ba3df1b616414757d4582ea4689c2b2d8d1cf78e
ojagggyo <ojagggyo@gmail.com>
2022-10-23 19:00:51 +0900

    m

--- sharp7/app.js
+++ sharp7/app.js
@@ -1,5 +1,5 @@
-const request = require('request')//非同期
-//const request = require('sync-request');//同期
+//const request = require('request')//非同期
+const request = require('sync-request');//同期
 //const request = require('request-promise');
 const fs = require('fs')
 const sharp = require('sharp')
