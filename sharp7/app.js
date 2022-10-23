58d2b32ea50402f01696dfd2d5df54dcbfdcdcf9
ojagggyo <ojagggyo@gmail.com>
2022-10-23 17:51:07 +0900

    m

--- sharp7/app.js
+++ sharp7/app.js
@@ -60,7 +60,7 @@ async function sub(tag, limit, urls){
             const optionsStart = {
                 uri: url,
                 method: "GET",
-                encoding: "binary", // it also works with encoding: null
+                encoding: null, // it also works with encoding: null
                 headers: {
                 }
             };
