2ba4f950f924945ebd791a57c3289b8c7fb03ecd
ojagggyo <ojagggyo@gmail.com>
2022-11-09 12:31:07 +0900

    m

--- 221110/app.js
+++ 221110/app.js
@@ -3,7 +3,7 @@ var { Client } = require('pg');
 
 var client = new Client({
     user: 'yasu',
-    host: '157.7.64.95',
+    host: '127.0.0.01',
     database: 'yasudb',
     password: 'yasuyasu',
     port: 5432
